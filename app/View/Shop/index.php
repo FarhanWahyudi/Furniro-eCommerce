4c3515e4ad0f237b32184283f53d9a043947ab13
farhan wahyudi <farhanwahyudi875@gmail.com>
2024-07-28 19:07:49 +0700

    feat: shop view

--- app/View/Shop/index.php
+++ app/View/Shop/index.php
@@ -1,3 +1,290 @@
-<div>
-    test
+<!-- NAVBAR -->
+<div class='navbar-container'>
+    <div class='logo-container'>
+        <img src="/Assets/Furniro-logo.png" alt="logo">
+        <h3>Furniro</h3>
+    </div>
+    <div class='link-container'>
+        <ul>
+            <li>Home</li>
+            <li>Shop</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+    <div class='icon-container'>
+        <ul>
+            <li>
+                <img src="/Assets/Icons/account.png" alt="account">
+            </li>
+            <li>
+                <img src="/Assets/Icons/search.png" alt="search">
+            </li>
+            <li>
+                <img src="/Assets/Icons/heart.png" alt="heart">
+            </li>
+            <li>
+                <img src="/Assets/Icons/shopping-cart.png" alt="shopping-cart">
+            </li>
+        </ul>
+    </div>
+</div>
+
+<!--  -->
+<div class="rectangle-container">
+    <h1>Shop</h1>
+    <div class="links">
+        <p><a class="home" href="">Home</a> > <a href="">Shop</a></p>
+    </div>
+</div>
+
+<!-- FILTER -->
+<div class="filter-container">
+    <div class="container">
+        <div class="form">
+            <div class="left-container">
+                <div class="filter">
+                    <img src="/Assets/Filter/filter.png" alt="filter">
+                    <p>Filter</p>
+                </div>
+                <img src="/Assets/Filter/grid-big-round.png" alt="">
+                <img src="/Assets/Filter/view-list.png" alt="">
+                <p class="line">|</p>
+                <p class="result">Showing 1-8 of 32 result</p>
+            </div>
+            <div class="right-container">
+                <div class="show">
+                    <label for="totalShow">Show</label>
+                    <select name="totalShow" id="totalShow">
+                        <option value="4">4</option>
+                        <option value="8">8</option>
+                        <option value="12">12</option>
+                        <option value="16">16</option>
+                    </select>
+                </div>
+                <div class="short">
+                    <label for="shortBy">ShortBy</label>
+                    <select name="shortBy" id="shortBy">
+                        <option value="default">Default</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- PRODUCTS -->
+<div class="products-container">
+    <div class="container">
+        <div class="list-product">
+            <div class="products">
+                <div class="card">
+                    <div class="img-container">
+                        <img src="/Assets/dummy-product.png" alt="product">
+                        <div class="label">-50%</div>
+                    </div>
+                    <div class="desc">
+                        <h3>Syltherine</h3>
+                        <p>Stylish cafe chair</p>
+                        <div class="price">
+                            <h4>Rp 2.500.000</h4>
+                            <h5>Rp 3.500.000</h5>
+                        </div>
+                    </div>
+                    <div class="pop-up">
+                        <button>Add to cart</button>
+                        <div class="links">
+                            <div class="link">
+                                <img src="/Assets/Popup/share.png" alt="share">
+                                <p>Share</p>
+                            </div>
+                            <div class="link">
+                                <img src="/Assets/Popup/compare.png" alt="compare">
+                                <p>Compare</p>
+                            </div>
+                            <div class="link">
+                                <img src="/Assets/Popup/heart.png" alt="heart">
+                                <p>Like</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img-container">
+                        <img src="/Assets/dummy-product.png" alt="product">
+                    </div>
+                    <div class="desc">
+                        <h3>Syltherine</h3>
+                        <p>Stylish cafe chair</p>
+                        <div class="price">
+                            <h4>Rp 2.500.000</h4>
+                            <h5>Rp 3.500.000</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img-container">
+                        <img src="/Assets/dummy-product.png" alt="product">
+                    </div>
+                    <div class="desc">
+                        <h3>Syltherine</h3>
+                        <p>Stylish cafe chair</p>
+                        <div class="price">
+                            <h4>Rp 2.500.000</h4>
+                            <h5>Rp 3.500.000</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img-container">
+                        <img src="/Assets/dummy-product.png" alt="product">
+                    </div>
+                    <div class="desc">
+                        <h3>Syltherine</h3>
+                        <p>Stylish cafe chair</p>
+                        <div class="price">
+                            <h4>Rp 2.500.000</h4>
+                            <h5>Rp 3.500.000</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img-container">
+                        <img src="/Assets/dummy-product.png" alt="product">
+                    </div>
+                    <div class="desc">
+                        <h3>Syltherine</h3>
+                        <p>Stylish cafe chair</p>
+                        <div class="price">
+                            <h4>Rp 2.500.000</h4>
+                            <h5>Rp 3.500.000</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img-container">
+                        <img src="/Assets/dummy-product.png" alt="product">
+                    </div>
+                    <div class="desc">
+                        <h3>Syltherine</h3>
+                        <p>Stylish cafe chair</p>
+                        <div class="price">
+                            <h4>Rp 2.500.000</h4>
+                            <h5>Rp 3.500.000</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img-container">
+                        <img src="/Assets/dummy-product.png" alt="product">
+                    </div>
+                    <div class="desc">
+                        <h3>Syltherine</h3>
+                        <p>Stylish cafe chair</p>
+                        <div class="price">
+                            <h4>Rp 2.500.000</h4>
+                            <h5>Rp 3.500.000</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img-container">
+                        <img src="/Assets/dummy-product.png" alt="product">
+                    </div>
+                    <div class="desc">
+                        <h3>Syltherine</h3>
+                        <p>Stylish cafe chair</p>
+                        <div class="price">
+                            <h4>Rp 2.500.000</h4>
+                            <h5>Rp 3.500.000</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="pagination-container">
+                <button class="page">1</button>
+                <button class="page">2</button>
+                <button class="page">3</button>
+                <button class="next-button">Next</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- FEATURES -->
+ <div class="features-container">
+    <div class="feature">
+        <img src="/Assets/Features/trophy.png" alt="trophy">
+        <div class="title">
+            <h2>Hight Quality</h2>
+            <p>crafted from top materials</p>
+        </div>
+    </div>
+    <div class="feature">
+        <img src="/Assets/Features/guarantee.png" alt="guarantee">
+        <div class="title">
+            <h2>Warranty Protection</h2>
+            <p>over 2 years</p>
+        </div>
+    </div>
+    <div class="feature">
+        <img src="/Assets/Features/shipping.png" alt="shipping">
+        <div class="title">
+            <h2>Free Shipping</h2>
+            <p>order over 150 $</p>
+        </div>
+    </div>
+    <div class="feature">
+        <img src="/Assets/Features/customer-support.png" alt="customer-support">
+        <div class="title">
+            <h2>24 / 7 Support</h2>
+            <p>dedicated support</p>
+        </div>
+    </div>
+ </div>
+
+ <!-- FOOTER -->
+ <div class="footer-container">
+    <div class="container">
+        <div class="footer">
+            <div class="brand-info">
+                <h2>Furniro.</h2>
+                <p>400 University Drive Suite 200 Coral Gables,</br>
+                FL 33134 USA</p>
+            </div>
+            <div class='links-container'>
+                <div class="links">
+                    <p>Links</p>
+                    <ul>
+                        <li>Home</li>
+                        <li>Shop</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </div>
+            <div class='help-container'>
+                <div class="help">
+                    <p>Help</p>
+                    <ul>
+                        <li>Payment Options</li>
+                        <li>Returns</li>
+                        <li>Privacy Policies</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="newsletter-container">
+                <div class="newsletter">
+                    <p>Newsletter</p>
+                    <div class="input">
+                        <input type="email" placeholder="Enter Your Email Address">
+                        <button>SUBSCRIBE</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyright">
+            <h5>2024 furniro. All rights reverved</h5>
+        </div>
+    </div>
 </div>
