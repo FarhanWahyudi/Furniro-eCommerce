f6bafebf521aefbac591d9e226a4ed3cdd3b72cf
farhan wahyudi <farhanwahyudi875@gmail.com>
2024-08-02 20:08:51 +0700

    feat: login view

--- app/View/Auth/login.php
+++ app/View/Auth/login.php
@@ -1,3 +1,23 @@
-<div>
-    hello
+<div class="box-container">
+    <div class="img">
+        <img src="/Assets/Auth/web-shopping.png" alt="">
+    </div>
+    <div class="login-container">
+        <div class="login">
+            <div class="title">
+                <h1>Welcome<br>Back</h1>
+                <p>Hey! Good to see you again</p>
+            </div>
+            <form action="">
+                <div class="email-form">
+                    <input type="email" placeholder="Email">
+                </div>
+                <div class="password-form">
+                    <input type="password" placeholder="Password">
+                </div>
+                <button type="submit">Sign In</button>
+            </form>
+            <p class="signup-link">Don't have an account? <a href="">Sign Up</a></p>
+        </div>
+    </div>
 </div>
